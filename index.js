1aa76202112b512bf13337589cd2e3cb8bee8abe
ruthnewman <ruthnewman@gmail.com>
2018-11-25 21:18:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   let array = [];
   for(let mCounter = 0; mCounter < musicians.length; mCounter++) {
     for (let iCounter = 0; iCounter < instruments.length; iCounter++) {
-    let string = musicians[mCounter] + " plays " + instruments[iCounter];
+    const string = musicians[mCounter] + " plays " + instruments[iCounter];
     array.push(string);
   }
   }
