2b2c70dc7e0a8933cb85efb64ffec8fd9b93ccf2
ruthnewman <ruthnewman@gmail.com>
2018-11-25 21:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  let array = [];
+  const array = [];
   for(let mCounter = 0; mCounter < musicians.length; mCounter++) {
     for (let iCounter = 0; iCounter < instruments.length; iCounter++) {
-    const string = musicians[mCounter] + " plays " + instruments[iCounter];
+    let string = musicians[mCounter] + " plays " + instruments[iCounter];
     array.push(string);
   }
   }
