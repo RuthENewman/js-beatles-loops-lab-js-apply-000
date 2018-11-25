9a4fbd966f8c6dfa81919f184db4637a6c0f11d9
ruthnewman <ruthnewman@gmail.com>
2018-11-25 21:05:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  
+  var [];
+  for(let counter = 0; counter < musicians.length; counter++) {
+    
+  }
 }
+
+
+function johnLennonFacts = facts
