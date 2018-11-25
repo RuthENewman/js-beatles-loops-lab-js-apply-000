54492db8002eb15531f02cbd1d12ca6a754f9e16
ruthnewman <ruthnewman@gmail.com>
2018-11-25 22:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
  var array = [];
 } do {
-  array.push("I love the Beatles!");
+  array[number] = "I love the Beatles!";
   number++;
 } while (number < 15); {
  return array;
