ff19d0685bbad253c1fc96248ffd675190953447
ruthnewman <ruthnewman@gmail.com>
2018-11-25 21:37:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function iLoveTheBeatles(number) {
 } do {
   array.push("I love the Beatles!");
   number++;
-} while (number < 15) {
-  return 
-}
+  console.log(array);
+} while (number < 15) 
 
